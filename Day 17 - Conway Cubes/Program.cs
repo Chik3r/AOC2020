ecf2ef210fe2e4b1b06d1f48940f9142d3ec0adc
Chik3r <ikervaraalarcon@gmail.com>
2020-12-18 19:42:01 -0300

    Day 17 - Part 2

--- Day 17 - Conway Cubes/Program.cs
+++ Day 17 - Conway Cubes/Program.cs
@@ -12,7 +12,7 @@ namespace Conway_Cubes
 		static void Main(string[] args)
 		{
 			var path = Path.Combine(Assembly.GetExecutingAssembly().Location, @"..\input.txt");
-			List<List<List<bool>>> pocketDim = new List<List<List<bool>>>(); // [z][y][x]
+			List<List<List<List<bool>>>> pocketDim = new List<List<List<List<bool>>>>(); // [z][y][x]
 			using (TextReader sr = new StreamReader(path, Encoding.UTF8))
 			{
 				string line;
@@ -29,40 +29,51 @@ namespace Conway_Cubes
 
 					input.Add(lineTypes);
 				}
-
-				pocketDim.Add(input);
+				List<List<List<bool>>> zInput = new List<List<List<bool>>>();
+				zInput.Add(input);
+				pocketDim.Add(zInput);
 			}
 
-			List<List<List<bool>>> modifiedDim = new List<List<List<bool>>>(); // [z][y][x]
-			var a = GetNearTiles(1, 1, 0, pocketDim);
+			List<List<List<List<bool>>>> modifiedDim = new List<List<List<List<bool>>>>(); // [z][y][x]
 
 			for (int i = 0; i < 6; i++) // Expand the space
 			{
 				modifiedDim = pocketDim.DeepCopyList();
 
-				List<bool> yList = new List<bool>(new bool[pocketDim.First().First().Count]);
+				List<bool> yList = new List<bool>(new bool[pocketDim.First().First().First().Count]);
 				List<List<bool>> zList = new List<List<bool>>();
+				for (int m = 0; m < pocketDim.First().First().Count; m++)
+					zList.Add(yList.DeepCopyList());
+				List<List<List<bool>>> wList = new List<List<List<bool>>>();
 				for (int m = 0; m < pocketDim.First().Count; m++)
-					zList.Add(yList.Select(k => k).ToList());
+					wList.Add(zList.DeepCopyList());
 
-				modifiedDim.Insert(0, zList.DeepCopyList()); // Expand the plane on the z axis
-				modifiedDim.Insert(modifiedDim.Count, zList.DeepCopyList());
-				pocketDim.Insert(0, zList.DeepCopyList());
-				pocketDim.Insert(pocketDim.Count, zList.DeepCopyList());
+				modifiedDim.Insert(0, wList.DeepCopyList()); // Expand the plane on the z axis
+				modifiedDim.Insert(modifiedDim.Count, wList.DeepCopyList());
+				pocketDim.Insert(0, wList.DeepCopyList());
+				pocketDim.Insert(pocketDim.Count, wList.DeepCopyList());
 
-				for (int z = 0; z < modifiedDim.Count; z++)
+				for (int w = 0; w < modifiedDim.Count; w++)
 				{
-					modifiedDim[z].Insert(0, yList.DeepCopyList());
-					modifiedDim[z].Insert(modifiedDim[z].Count, yList.DeepCopyList());
-					pocketDim[z].Insert(0, yList.DeepCopyList());
-					pocketDim[z].Insert(pocketDim[z].Count, yList.DeepCopyList());
+					modifiedDim[w].Insert(0, zList.DeepCopyList()); // Expand the plane on the z axis
+					modifiedDim[w].Insert(modifiedDim[w].Count, zList.DeepCopyList());
+					pocketDim[w].Insert(0, zList.DeepCopyList());
+					pocketDim[w].Insert(pocketDim[w].Count, zList.DeepCopyList());
 
-					for (int y = 0; y < modifiedDim[z].Count; y++)
+					for (int z = 0; z < modifiedDim[w].Count; z++)
 					{
-						modifiedDim[z][y].Insert(0, false);
-						modifiedDim[z][y].Insert(modifiedDim[z][y].Count, false);
-						pocketDim[z][y].Insert(0, false);
-						pocketDim[z][y].Insert(pocketDim[z][y].Count, false);
+						modifiedDim[w][z].Insert(0, yList.DeepCopyList());
+						modifiedDim[w][z].Insert(modifiedDim[w][z].Count, yList.DeepCopyList());
+						pocketDim[w][z].Insert(0, yList.DeepCopyList());
+						pocketDim[w][z].Insert(pocketDim[w][z].Count, yList.DeepCopyList());
+
+						for (int y = 0; y < modifiedDim[w][z].Count; y++)
+						{
+							modifiedDim[w][z][y].Insert(0, false);
+							modifiedDim[w][z][y].Insert(modifiedDim[w][z][y].Count, false);
+							pocketDim[w][z][y].Insert(0, false);
+							pocketDim[w][z][y].Insert(pocketDim[w][z][y].Count, false);
+						}
 					}
 				}
 
@@ -73,18 +84,21 @@ namespace Conway_Cubes
 			{
 				modifiedDim = pocketDim.DeepCopyList();
 
-				for (int z = 0; z < modifiedDim.Count; z++)
+				for (int w = 0; w < modifiedDim.Count; w++)
 				{
-					for (int y = 0; y < modifiedDim[z].Count; y++)
+					for (int z = 0; z < modifiedDim[w].Count; z++)
 					{
-						for (int x = 0; x < modifiedDim[z][y].Count; x++)
+						for (int y = 0; y < modifiedDim[w][z].Count; y++)
 						{
-							int nearTiles = GetNearTiles(x, y, z, pocketDim);
+							for (int x = 0; x < modifiedDim[w][z][y].Count; x++)
+							{
+								int nearTiles = GetNearTiles(x, y, z, w, pocketDim);
 
-							if (pocketDim[z][y][x] && !(nearTiles == 2 || nearTiles == 3))
-								modifiedDim[z][y][x] = false;
-							else if (!pocketDim[z][y][x] && nearTiles == 3)
-								modifiedDim[z][y][x] = true;
+								if (pocketDim[w][z][y][x] && !(nearTiles == 2 || nearTiles == 3))
+									modifiedDim[w][z][y][x] = false;
+								else if (!pocketDim[w][z][y][x] && nearTiles == 3)
+									modifiedDim[w][z][y][x] = true;
+							}
 						}
 					}
 				}
@@ -92,31 +106,35 @@ namespace Conway_Cubes
 				pocketDim = modifiedDim.DeepCopyList();
 			}
 
-			int activeCubes = pocketDim.Sum(z => z.Sum(y => y.Count(x => x)));
+			int activeCubes = pocketDim.Sum(w => w.Sum(z => z.Sum(y => y.Count(x => x))));
 			Console.WriteLine(activeCubes);
 		}
 
-		static int GetNearTiles(int x, int y, int z, List<List<List<bool>>> input)
+		static int GetNearTiles(int x, int y, int z, int w, List<List<List<List<bool>>>> input)
 		{
 			int totalNeighbours = 0;
 
-			for (int currX = -1; currX < 2; currX++)
+			for (int currW = -1; currW < 2; currW++)
 			{
-				for (int currY = -1; currY < 2; currY++)
+				for (int currX = -1; currX < 2; currX++)
 				{
-					for (int currZ = -1; currZ < 2; currZ++)
+					for (int currY = -1; currY < 2; currY++)
 					{
-						int currentX = x + currX;
-						int currentY = y + currY;
-						int currentZ = z + currZ;
-
-						if (currentX == x && currentY == y && currentZ == z)
-							continue;
-
-						if (currentX >= 0 && currentY >= 0 && currentZ >= 0 && currentZ < input.Count() && currentY < input[currentZ].Count() && currentX < input[currentZ][currentY].Count)
+						for (int currZ = -1; currZ < 2; currZ++)
 						{
-							if (input[currentZ][currentY][currentX] == true)
-								totalNeighbours++;
+							int currentX = x + currX;
+							int currentY = y + currY;
+							int currentZ = z + currZ;
+							int currentW = w + currW;
+
+							if (currentX == x && currentY == y && currentZ == z && currentW == w)
+								continue;
+
+							if (currentX >= 0 && currentY >= 0 && currentZ >= 0 && currentW >= 0 && currentW < input.Count && currentZ < input[currentW].Count() && currentY < input[currentW][currentZ].Count() && currentX < input[currentW][currentZ][currentY].Count)
+							{
+								if (input[currentW][currentZ][currentY][currentX] == true)
+									totalNeighbours++;
+							}
 						}
 					}
 				}
@@ -128,6 +146,11 @@ namespace Conway_Cubes
 
 	static class ListUtils
 	{
+		public static List<List<List<List<bool>>>> DeepCopyList(this List<List<List<List<bool>>>> list)
+		{
+			return list.Select(w => w.Select(z => z.Select(y => y.Select(x => x).ToList()).ToList()).ToList()).ToList();
+		}
+
 		public static List<List<List<bool>>> DeepCopyList(this List<List<List<bool>>> list)
 		{
 			return list.Select(z => z.Select(y => y.Select(x => x).ToList()).ToList()).ToList();
