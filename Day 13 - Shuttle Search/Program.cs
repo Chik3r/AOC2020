2c0c902482460542adfb78698e628d9bfedf2788
Chik3r <ikervaraalarcon@gmail.com>
2020-12-21 14:54:42 -0300

    Day 13 - Part two

--- Day 13 - Shuttle Search/Program.cs
+++ Day 13 - Shuttle Search/Program.cs
@@ -50,6 +50,45 @@ namespace Shuttle_Search
 
 			KeyValuePair<Bus, int> minValue = input.Aggregate((l, r) => l.Value < r.Value ? l : r);
 			Console.WriteLine($"Result: {(minValue.Value - targetTime) * minValue.Key.ID}");
+
+			// -------- Part Two --------
+			int dictIndex = 0;
+			Dictionary<int, Bus> newInput = input.Select(x => x.Key).ToDictionary(x => dictIndex++);
+			List<long> resultOperations = new List<long>(new long[newInput.Count]);
+			for (int i = 0; i < newInput.Count; i++)
+			{
+				for (int x = 0; x < newInput.Count; x++)
+				{
+					if (x == i)
+						continue;
+
+					if (resultOperations[x] != 0)
+						resultOperations[x] *= (long)newInput[i].ID;
+					else
+						resultOperations[x] = (long)newInput[i].ID;
+				}
+			}
+
+			for (int i = 0; i < newInput.Count; i++)
+			{
+				long moduloResult = (resultOperations[i] + newInput[i].offset) % newInput[i].ID;
+
+				if (moduloResult == 0)
+					continue;
+
+				for (long m = 2; m < Math.Pow(10, 12); m++)
+				{
+					long mod = ((resultOperations[i] * m) + (long)newInput[i].offset) % (long)newInput[i].ID;
+					if (mod == 0)
+					{
+						resultOperations[i] *= m;
+						break;
+					}
+				}
+			}
+
+			Console.WriteLine(resultOperations.Sum());
+			Console.WriteLine(resultOperations.Sum() % newInput.Aggregate((long)1, (acc, val) => acc * (long)val.Value.ID));
 		}
 
 		class Bus
